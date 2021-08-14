6ab0436396b4b45dcb866eb5049f961cb344e01e
garammasala29 <kmasa1569@gmail.com>
2021-08-14 16:32:50 +0900

    create users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/index'
+  get 'users/show'
+  root 'books#index'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    passwords: 'users/passwords'
+  }
+  resources :users, only: [:index, :show]
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
