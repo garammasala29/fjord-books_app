daa8d9f03fff1f6ccb4c2895fad5b82e54d08813
garammasala29 <kmasa1569@gmail.com>
2021-08-16 21:56:08 +0900

    add authenticate_user_filter

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  skip_before_action :authenticate_user!, only: [:new, :create]
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   def new
