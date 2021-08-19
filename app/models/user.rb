17b181416cc74220416b1ebb5ce74bf445265c27
garammasala29 <kmasa1569@gmail.com>
2021-08-20 01:18:25 +0900

    introduce image_processing

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   has_one_attached :image
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def display_image
+    image.variant(resize_to_limit: [400, 400])
+  end
+
+  def display_image_mini
+    image.variant(resize_to_limit: [100, 100])
+  end
 end
