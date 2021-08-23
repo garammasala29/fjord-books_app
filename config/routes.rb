033816b4b9bce41fd5a20bd0f72a765d4c84e8aa
garammasala29 <kmasa1569@gmail.com>
2021-08-23 14:21:39 +0900

    update controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   root 'books#index'
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions',
-    passwords: 'users/passwords'
-  }
+  devise_for :users
   resources :users, only: [:index, :show]
   resources :books
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
