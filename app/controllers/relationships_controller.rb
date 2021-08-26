684036e67f2d4272e10ed85548c74378f8b450c8
garammasala29 <kmasa1569@gmail.com>
2021-08-26 21:34:12 +0900

    Fix rubocop warnings

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 class RelationshipsController < ApplicationController
   def create
     following = current_user.active_relationships.build(follower_id: params[:follower_id])
     following.save
-    redirect_to request.referrer
+    redirect_to request.referer
   end
 
   def destroy
     following = Relationship.find_by(following_id: current_user.id, follower_id: params[:id])
     following.destroy
-    redirect_to request.referrer
+    redirect_to request.referer
   end
 end
