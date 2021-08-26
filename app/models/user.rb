6be5580622acc23ce677a964523df601c85ef848
garammasala29 <kmasa1569@gmail.com>
2021-08-27 07:59:48 +0900

    Rename avatar -> image & configure_permitted_extensions -> validate_image_permission

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,22 +2,22 @@
 
 class User < ApplicationRecord
   has_one_attached :image
-  validate :configure_permitted_extensions
+  validate :validate_image_permission
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  def avatar
+  def middle_image
     image.variant(resize_to_limit: [200, 200])
   end
 
-  def small_avatar
+  def small_image
     image.variant(resize_to_limit: [50, 50])
   end
 
-  def configure_permitted_extensions
+  def validate_image_permission
     return unless image.attached?
 
-    extension = ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']
-    errors.add(:image, I18n.t('message.incorrect_extension')) unless image.content_type.in?(extension)
+    extensions = ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']
+    errors.add(:image, I18n.t('message.incorrect_extension')) unless image.content_type.in?(extensions)
   end
 end
