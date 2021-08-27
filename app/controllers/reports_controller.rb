040a8ecd6dd551c2ac6fdef42fe56da22eef6fb8
garammasala29 <kmasa1569@gmail.com>
2021-08-27 22:55:29 +0900

    Use i18n for report_views

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,7 +2,7 @@ class ReportsController < ApplicationController
   before_action :set_report, only: %i[ show edit update destroy ]
 
   def index
-    @reports = Report.all
+    @reports = Report.order(:id).page(params[:page])
   end
 
   def show
@@ -18,23 +18,23 @@ class ReportsController < ApplicationController
   def create
     @report = Report.new(report_params)
     if @report.save
-      redirect_to @report, notice: "Report was successfully created."
+      redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
     else
-      render :new, status: :unprocessable_entity
+      render :new
     end
   end
 
   def update
     if @report.update(report_params)
-      redirect_to @report, notice: "Report was successfully updated."
+      redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
-      render :edit, status: :unprocessable_entity
+      render :edit
     end
   end
 
   def destroy
     @report.destroy
-    redirect_to reports_url, notice: "Report was successfully destroyed."
+    redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
   end
 
   private
