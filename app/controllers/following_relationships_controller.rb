8a47e602e151c82016fa05ff3bb1423fcdd2b911
garammasala29 <kmasa1569@gmail.com>
2021-09-02 09:38:29 +0900

    Fix redirect path

--- app/controllers/following_relationships_controller.rb
+++ app/controllers/following_relationships_controller.rb
@@ -1,14 +1,20 @@
 # frozen_string_literal: true
 
 class FollowingRelationshipsController < ApplicationController
+  before_action :set_user
+
   def create
-    current_user.active_relationships.create(follower_id: params[:follower_id])
-    redirect_to request.referer
+    current_user.active_relationships.create!(follower_id: params[:id])
+    redirect_to user_path(@user)
   end
 
   def destroy
     following = FollowingRelationship.find_by(following_id: current_user.id, follower_id: params[:id])
     following.destroy
-    redirect_to request.referer
+    redirect_to user_path(@user)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
