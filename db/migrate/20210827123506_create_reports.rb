133255f851bf3bbc493168ec2248e4561cbc4d42
garammasala29 <kmasa1569@gmail.com>
2021-09-03 10:14:04 +0900

    Fix migration_file

--- db/migrate/20210827123506_create_reports.rb
+++ db/migrate/20210827123506_create_reports.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class CreateReports < ActiveRecord::Migration[6.1]
   def change
     create_table :reports do |t|
-      t.references :user, null:false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.string :title, null: false
       t.text :content, null: false
 
