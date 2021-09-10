65baed5aaf083e1c741fc5b3749826c154e3ccae
garammasala29 <kmasa1569@gmail.com>
2021-09-10 10:53:33 +0900

    Add user class test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,41 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @alice = users(:alice)
+    @bob = users(:bob)
+  end
+
+  test '#following?' do
+    assert_not @alice.following?(@bob)
+    @alice.active_relationships.create(following_id: @bob.id)
+    assert @alice.following?(@bob)
+  end
+
+  test '#followed_by?' do
+    assert_not @alice.following?(@bob)
+    @alice.active_relationships.create(following_id: @bob.id)
+    assert @bob.followed_by?(@alice)
+  end
+
+  test '#follow' do
+    assert_not @alice.following?(@bob)
+    @alice.follow(@bob)
+    assert @alice.following?(@bob)
+  end
+
+  test '#unfollow' do
+    assert_not @alice.following?(@bob)
+    @alice.follow(@bob)
+    assert @alice.following?(@bob)
+    @alice.unfollow(@bob)
+    assert_not @alice.following?(@bob)
+  end
+
+  test '#name_or_email' do
+    user = User.new(email: 'foo@example.com', name: '')
+    assert_equal 'foo@example.com', user.name_or_email
+    user.name = 'garammasala'
+    assert_equal 'garammasala', user.name_or_email
+  end
 end
