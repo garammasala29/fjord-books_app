0746225817a4f3ed796eae33230705e558c02487
garammasala29 <kmasa1569@gmail.com>
2021-09-10 10:54:11 +0900

    Add book system test

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -4,44 +4,60 @@ require 'application_system_test_case'
 
 class BooksTest < ApplicationSystemTestCase
   setup do
-    @book = books(:one)
+    @book = books(:webtechbook)
+    visit root_url
+    fill_in 'Eメール',	with: 'alice@example.com'
+    fill_in 'パスワード',	with: 'password'
+    click_button 'ログイン'
   end
 
   test 'visiting the index' do
     visit books_url
-    assert_selector 'h1', text: 'Books'
+    assert_selector 'h1', text: '本'
+  end
+
+  test 'showing a book' do
+    visit book_url(@book)
+    assert_selector 'h1', text: '本の詳細'
+    assert_text @book.title
+    assert_text @book.memo
+    assert_text @book.author
   end
 
   test 'creating a Book' do
     visit books_url
-    click_on 'New Book'
-
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Create Book'
-
-    assert_text 'Book was successfully created'
-    click_on 'Back'
+    click_on '新規作成'
+    fill_in 'タイトル', with: @book.title
+    fill_in 'メモ', with: @book.memo
+    fill_in '著者', with: @book.author
+    click_on '登録する'
+    assert_text '本が作成されました。'
+    assert_text @book.title
+    assert_text @book.memo
+    assert_text @book.author
   end
 
   test 'updating a Book' do
     visit books_url
-    click_on 'Edit', match: :first
-
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Update Book'
-
-    assert_text 'Book was successfully updated'
-    click_on 'Back'
+    click_on '編集'
+    fill_in 'タイトル', with: 'タイトル変更'
+    fill_in 'メモ', with: 'メモ変更'
+    fill_in '著者', with: '著者変更'
+    click_on '更新する'
+    assert_text '本が更新されました'
+    assert_text 'タイトル変更'
+    assert_text 'メモ変更'
+    assert_text '著者変更'
   end
 
   test 'destroying a Book' do
     visit books_url
     page.accept_confirm do
-      click_on 'Destroy', match: :first
+      click_on '削除'
     end
-
-    assert_text 'Book was successfully destroyed'
+    assert_text '本が削除されました'
+    assert_no_text @book.title
+    assert_no_text @book.memo
+    assert_no_text @book.author
   end
 end
