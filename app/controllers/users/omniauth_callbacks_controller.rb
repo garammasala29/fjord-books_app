9879fffcb5593a2a34ccabd7cdcbbe87afe72ae5
garammasala29 <kmasa1569@gmail.com>
2022-05-02 23:36:25 +0900

    rubocopに対応

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,6 @@
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  skip_before_action :verify_authenticity_token, only: :create
+# frozen_string_literal: true
 
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.from_omniauth(request.env['omniauth.auth'])
     sign_in_and_redirect @user, event: :authentication
