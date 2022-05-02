47a829d8f6fb842056517f3a74ccbd74224fcbf4
garammasala29 <kmasa1569@gmail.com>
2022-05-03 00:26:20 +0900

    テストデータを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,6 +52,7 @@ User.destroy_all
     name: name,
     postal_code: "123-#{n.to_s.rjust(4, '0')}",
     address: Faker::Address.full_address,
+    uid: User.create_unique_string,
     self_introduction: "こんにちは、#{name}です。"
   )
 end
